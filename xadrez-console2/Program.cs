3ad1e4d5c04f9621b64849145f211e20ae663729
Eduardo Alves <eduardo.lves@outlook.com>
2023-07-28 10:53:12 -0300

    Preparando o programa para testes iniciais

--- xadrez-console2/Program.cs
+++ xadrez-console2/Program.cs
@@ -4,19 +4,13 @@ using xadrez;
 
 try
 {
-    Tabuleiro tab = new Tabuleiro(8, 8);
+    PartidaDeXadrez partida = new PartidaDeXadrez();
 
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 4));
-
-    Tela.imprimirTabuleiro(tab);
-
-    Console.WriteLine();
+    Tela.imprimirTabuleiro(partida.tab);
 }
 catch (TabuleiroException e)
 {
     Console.WriteLine(e.Message);
 }
+
+Console.ReadLine();
